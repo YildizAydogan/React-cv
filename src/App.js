b08117c738d50ecdfad981b2b567934c2f56dc6f
YildizAydogan <yildizaydoganjobmail@gmail.com>
2022-04-21 23:16:33 +0200

    2022

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './componenets/header/header';
+import Slider from './componenets/slider/slider';
+import Intro from './componenets/intro/intro';
+import About from './componenets/about/about';
+import Quote from './componenets/quote/quote';
+import Skills from './componenets/skills/skills';
+
+
+
+function App() {
+  return (
+   <>
+   <Header/>
+   <Slider/> 
+   <Intro/>
+   <About/>
+   <Quote/>
+   <Skills/>
+   
+   
+   
+   </>
+  );
+}
+
+export default App;
